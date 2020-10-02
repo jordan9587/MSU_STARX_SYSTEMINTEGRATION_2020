fe2e6dd2292ab319516b4109610c2d1dffbfef80
Jordan Hybki <hybkijor@msu.edu>
2020-10-02 15:07:03 -0400

    FSM and Torsion Sensor Library Changes
    For FSM, changed library properties to any architecture to stop asm vs arm error when working in arudino due programming port.
    
    Removed interrupt functionality for torsionSensor library.

--- Arduino/libraries/torsionSensor/torsionSensor.cpp
+++ Arduino/libraries/torsionSensor/torsionSensor.cpp
@@ -10,31 +10,37 @@
 
 torsionSensor::torsionSensor(int pin0, int pin1)
 {
-   // Assume mPinSensor0 is 13 and mPinSensor1 is 2. Since registry 
-   // Manipulation is for those ports.
-   mPinSensor0 = pin0;
-   mPinSensor1 = pin1;
+    /*
+     Assume mPinSensor0 is 13 and mPinSensor1 is 2. Since registry
+     Manipulation is for those ports. Pins send Quadrature-encoded sensor
+     signal.
+    */
+    mPinSensor0 = pin0;
+    mPinSensor1 = pin1;
 
-   // Set pullups on torison inputs
-   pinMode(mPinSensor1, OUTPUT);
-   pinMode(mPinSensor0, OUTPUT);
-   digitalWrite(mPinSensor1, 1);
-   digitalWrite(mPinSensor0, 1);
+    /*
+     Set pullups on torison inputs.
+    */
+    pinMode(mPinSensor1, OUTPUT);
+    pinMode(mPinSensor0, OUTPUT);
+    digitalWrite(mPinSensor1, 1);
+    digitalWrite(mPinSensor0, 1);
+    // long may be a 64-bit type, but 1 is still an int. You need to make 1 a long int using the UL suffix.
+    REG_PMC_PCER0 = (1UL<<27); // Activate clock for TC0
+    // Reset counter on TC_CV == RC
+    REG_TC0_CMR0 = (1<<0)|(1<<2)|(1<<8)|(1<<10)|(1 << 14);    // reset counter on index
+    REG_TC0_BMR = (1<<8)|(1<<9)|(1<<12);     // activate with no filters
+    REG_TC0_CCR0 = 5;    //enable clock and reset counter
 
-   // long may be a 64-bit type, but 1 is still an int. You need to make 1 a long int using the UL suffix.
-   REG_PMC_PCER0 = (1UL<<27); // Activate clock for TC0
-   // Reset counter on TC_CV == RC
-   REG_TC0_CMR0 = (1<<0)|(1<<2)|(1<<8)|(1<<10)|(1 << 14);    // reset counter on index
-   REG_TC0_BMR = (1<<8)|(1<<9)|(1<<12);     // activate with no filters
-   REG_TC0_CCR0 = 5;    //enable clock and reset counter
-
-   REG_TC0_RC0 = 360;
-   
-   Serial.begin(9600);
+    REG_TC0_RC0 = 360;
+    
+    Serial.begin(9600);
+    
 }
 
 
-void torsionSensor::loopTorsion()
+
+void torsionSensor::loopTorsionOutput()
 {
     // put your main code here, to run repeatedly:
     Serial.println(REG_TC0_CV0); 
--- Arduino/libraries/torsionSensor/torsionSensor.h
+++ Arduino/libraries/torsionSensor/torsionSensor.h
@@ -21,12 +21,20 @@ class torsionSensor
     /*
     * Loop required to read register and print current rotation.
     */
-    void loopTorsion();
-
+    void loopTorsionOutput();
+    
+  protected:
+    // track last position so we know whether it's worth printing new output
+    long previous_position = 0;
+    
   private:
+    // Registry Manipulation Constants:
     // Assume mPinSensor0 is 13 and mPinSensor1 is 2.
     int mPinSensor0;    // Analog pin 0 for left torsion sensor
     int mPinSensor1;    // Analog pin 1 for right torsion sensor
+
+
+
 };
 
 #endif
